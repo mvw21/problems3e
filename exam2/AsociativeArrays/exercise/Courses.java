b87b4b3ac1291dd6eb5806039fbba7427e5bb68a
mvw21 <53233579+mvw21@users.noreply.github.com>
2019-08-01 19:47:45 +0300

    Update Courses.java

--- exam2/AsociativeArrays/exercise/Courses.java
+++ exam2/AsociativeArrays/exercise/Courses.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+// MAP ot STRING i List !
 
 public class Courses {
     public static void main(String[] args) throws IOException {
